f232dc9ba43812a49f79f48e8e0078ee168b8323
chirumer <chirumer@users.noreply.github.com>
2021-09-05 20:29:13 +0530

    added mascot + functionized resize_background

--- client/main.py
+++ client/main.py
@@ -2,6 +2,23 @@ import tkinter
 from PIL import ImageTk, Image
 
 
+def resize_background(background_img, window_width, window_height):
+    img_w, img_h = background_img.size
+    img_aspct = img_w/img_h
+    needed_width_w = window_width - img_w
+    needed_width_h = (window_height - img_h) * img_aspct
+    new_width = (needed_width_w if needed_width_w > needed_width_h else
+                 needed_width_h) + img_w
+    new_height = int(new_width//img_aspct)
+    background_img = background_img.resize((new_width, new_height), Image.ANTIALIAS)
+    img_w, img_h = background_img.size
+    background_img = background_img.crop((img_w/2 - window_width/2,
+                                          img_h/2 - window_height/2,
+                                          img_w/2 + window_width/2,
+                                          img_h/2 + window_height/2))
+    return background_img
+
+
 def get_username():
 
     # constants
@@ -18,19 +35,7 @@ def get_username():
     background_img = Image.open('background.jpg')
 
     # resize and crop image to fit window
-    img_w, img_h = background_img.size
-    img_aspct = img_w/img_h
-    needed_width_w = window_width - img_w
-    needed_width_h = (window_height - img_h) * img_aspct
-    new_width = (needed_width_w if needed_width_w > needed_width_h else
-                 needed_width_h) + img_w
-    new_height = int(new_width/img_aspct)
-    background_img = background_img.resize((new_width, new_height), Image.ANTIALIAS)
-    img_w, img_h = background_img.size
-    background_img = background_img.crop((img_w/2 - window_width/2,
-                                          img_h/2 - window_height/2,
-                                          img_w/2 + window_width/2,
-                                          img_h/2 + window_height/2))
+    background_img = resize_background(background_img, window_width, window_height)
 
     # insert background image to window
     background_img = ImageTk.PhotoImage(background_img)
