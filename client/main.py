ede182518d29ede458e0a56610276988c5696d72
chirumer <chirumer@users.noreply.github.com>
2021-09-05 17:19:29 +0530

    create simple 1000x500 window

--- client/main.py
+++ client/main.py
@@ -1,14 +1,16 @@
-# simple client send and receive
+import tkinter
 
-import socket
+def get_username():
 
-PORT = 5200
+    window_width = 1000
+    window_height = 500
+    aspect_ratio = window_width/window_height
+        # later: handle user resizes
 
-sock = socket.socket()
+    root = tkinter.Tk()
+    root.geometry(f'{window_width}x{window_height}')
 
-sock.connect(('127.0.0.1', PORT))
+    root.mainloop()
 
-sock.send('hello world'.encode())
-print(sock.recv(1024).decode())
-
-sock.close()
+username = get_username()
+    # later: change to user authentication page
