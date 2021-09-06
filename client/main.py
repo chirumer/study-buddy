9b2b2ec014d6985bfc3ef9d15a9f4e6d021e4dd1
chirumer <chirumer@users.noreply.github.com>
2021-09-06 19:43:57 +0530

    group creation

--- client/main.py
+++ client/main.py
@@ -16,6 +16,17 @@ parameters['aspect_ratio'] = (parameters['window_width'] /
 
 def username_page(base, temp_assets):
 
+    def cleanup():
+        temp_assets.clear()
+        base.canvas.delete(username_txt)
+        base.canvas.delete(go_btn)
+        entry.destroy()
+
+    def is_valid(username):
+        if not username:
+            return "Empty username"
+        return True
+
     username_img = tkinter.PhotoImage(file='username.png')
     temp_assets.append(username_img)
     username_txt = base.canvas.create_image(130, 180, image=username_img, anchor=tkinter.NW)
@@ -28,12 +39,13 @@ def username_page(base, temp_assets):
     go_btn = base.canvas.create_image(140, 285, image=go_img, anchor=tkinter.NW)
 
     def submit(event):
-        username = entry.get()
-        temp_assets.clear()
-        base.canvas.delete(username_txt)
-        base.canvas.delete(go_btn)
-        entry.destroy()
+        username = entry.get().strip()
 
+        if is_valid(username) != True:
+            popup_message('Alert!', 
+                          f'Invalid username.\nReason: {is_valid(username)}')
+            return
+        cleanup()
         user_login(base, username, temp_assets)
 
     base.canvas.tag_bind(go_btn, '<Button-1>', submit)
@@ -45,7 +57,8 @@ def user_login(base, username, temp_assets):
 
     if not is_success:
             # later: implement error handling
-        pass
+        poppup_message('Alert!', 'socket error')
+        return
 
     category_selection_page(base, temp_assets)
 
@@ -93,7 +106,86 @@ def existing_page(base, temp_assets):
     print('existing page')
 
 def create_page(base, temp_assets):
-    print('create page')
+
+    def cleanup():
+        temp_assets.clear()
+        title_entry.destroy()
+        tags_entry.destroy()
+        goals_entry.destroy()
+        base.canvas.delete(book_title_txt)
+        base.canvas.delete(tags_entry_txt)
+        base.canvas.delete(goals_txt)
+        base.canvas.delete(create_btn)
+        base.canvas.delete(back_btn)
+
+    def is_valid(title, tags, goals):
+        if not title:
+            return "Empty title field!"
+        elif not tags:
+            return "Empty tags field!"
+        elif not goals:
+            return "Empty goals field!"
+        return True
+
+    book_title_img = tkinter.PhotoImage(file='book_title.png')
+    temp_assets.append(book_title_img)
+    book_title_txt = base.canvas.create_image(80, 30, image=book_title_img, anchor=tkinter.NW)
+
+    title_entry = tkinter.Entry(base.canvas, width=17, font=('default', 15))
+    title_entry.place(x=90, y=90)
+
+    tags_entry_img = tkinter.PhotoImage(file='tags.png')
+    temp_assets.append(tags_entry_img)
+    tags_entry_txt= base.canvas.create_image(80, 150, image=tags_entry_img, anchor=tkinter.NW)
+
+    tags_entry = tkinter.Text(base.canvas, width=17, height=2, font=('default', 15))
+    tags_entry.place(x=90, y=205)
+
+    goals_img = tkinter.PhotoImage(file='group_goals.png')
+    temp_assets.append(goals_img)
+    goals_txt = base.canvas.create_image(80, 280, image=goals_img, anchor=tkinter.NW)
+
+    goals_entry = tkinter.Text(base.canvas, width=20, height=5, font=('default', 15))
+    goals_entry.place(x=90, y=340)
+
+    create_img = tkinter.PhotoImage(file='study_create.png')
+    temp_assets.append(create_img)
+    create_btn = base.canvas.create_image(400, 190, image=create_img, anchor=tkinter.NW)
+
+    def clicked_create(event):
+        title = title_entry.get().strip()
+        tags = tags_entry.get('1.0', tkinter.END).strip()
+        goals = goals_entry.get('1.0', tkinter.END).strip()
+
+        if is_valid(title, tags, goals) != True:
+            popup_message('Alert!',
+                          f'Error parsing fields.\nReason: {is_valid(title, tags, goals)}')
+            return
+
+        cleanup()
+            # later: mascot notifies success
+
+        is_success = server_interface.create_page(title, tags, goals)
+        if not is_success:
+                # later: error handling
+            poppup_message('Alert!', 'socket error')
+            return
+
+        category_selection_page(base, temp_assets)
+
+    base.canvas.tag_bind(create_btn, '<Button-1>', clicked_create)
+
+    back_img = tkinter.PhotoImage(file='back.png')
+    temp_assets.append(back_img)
+    back_btn = base.canvas.create_image(900, 20, image=back_img, anchor=tkinter.NW)
+
+    def clicked_back(event):
+        cleanup()
+        category_selection_page(base, temp_assets)
+
+    base.canvas.tag_bind(back_btn, '<Button-1>', clicked_back)
+
+
 
 def join_page(base, temp_assets):
     print('join page')
--- client/server_interface.py
+++ client/server_interface.py
@@ -1,2 +1,9 @@
 def login(username):
+    print(f'username: {username}')
+    return True
+
+def create_page(title, tags, goals):
+    print(f'title: {title}')
+    print(f'tags: {tags}')
+    print(f'goals: {goals}')
     return True
