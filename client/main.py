7fb9462c4c4fa1d27c4dd7557c995b705f1eb0bf
chirumer <chirumer@users.noreply.github.com>
2021-09-07 04:29:10 +0530

    added chat

--- client/main.py
+++ client/main.py
@@ -4,6 +4,8 @@ import server_interface
 import tkinter
 from PIL import ImageTk, Image, ImageFont, ImageDraw
 
+username = ''
+
 parameters = {
     'MILLISEC_PER_FRAME': 50,
     'mascot_path': 'mascot.gif',
@@ -41,6 +43,8 @@ def username_page(base, temp_assets):
     go_btn = base.canvas.create_image(140, 285, image=go_img, anchor=tkinter.NW)
 
     def submit(event):
+        global username
+
         username = entry.get().strip()
 
         if is_valid(username) != True:
@@ -103,19 +107,6 @@ def category_selection_page(base, temp_assets):
 
     base.canvas.tag_bind(join_btn, '<Button-1>', clicked_join)
 
-
-def existing_page(base, temp_assets):
-    groups = server_interface.get_groups()
-
-    chat = server_interface.get_chat(0)
-    print(chat)
-    print('end')
-
-    server_interface.send_chat(0, 'hi there\n')
-
-    chat = server_interface.get_chat(0)
-    print(chat)
-    print('end')
 
 
 def create_page(base, temp_assets):
@@ -312,9 +303,6 @@ def join_page(base, temp_assets):
     details_img = tkinter.PhotoImage(file='details.png')
     temp_assets.append(details_img)
 
-    join_img = tkinter.PhotoImage(file='join_group.png')
-    temp_assets.append(join_img)
-
     already_img = tkinter.PhotoImage(file='already_joined.png')
     temp_assets.append(already_img)
 
@@ -356,7 +344,7 @@ def join_page(base, temp_assets):
     temp_assets.append(txt_imgs)
     txt_ids = []
 
-    text = 'monkey adventures and donkey dancing'
+    text = 'loading..'
     create_text_image(base.canvas, text, 0, txt_imgs, txt_ids)
     create_text_image(base.canvas, text, 1, txt_imgs, txt_ids)
     create_text_image(base.canvas, text, 2, txt_imgs, txt_ids)
@@ -399,6 +387,234 @@ def join_page(base, temp_assets):
     groups = server_interface.get_groups()
     current_page_number = 0
     display_page(current_page_number, groups)
+
+# def existing_page(base, temp_assets):
+#     groups = server_interface.get_groups()
+# 
+#     chat = server_interface.get_chat(0)
+#     print(chat)
+#     print('end')
+# 
+#     server_interface.send_chat(0, 'hi there\n')
+# 
+#     chat = server_interface.get_chat(0)
+#     print(chat)
+#     print('end')
+
+def existing_page(base, temp_assets):
+
+    def cleanup():
+        temp_assets.clear()
+        txt_imgs.clear()
+
+        for detail_btn in detail_btns:
+            base.canvas.delete(detail_btn)
+
+        for open_btn in open_btns:
+            base.canvas.delete(open_btn)
+
+        for empty_txt in empty_txts:
+            base.canvas.delete(empty_txt)
+
+
+    def create_text_image(canvas, text, index, txt_imgs, txt_ids):
+
+        img = Image.new('RGBA', (400, 30), (0, 255, 0, 0))
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('montserrat.otf', 25)
+
+        if draw.textsize(text, font=font)[0] > 300:
+            while draw.textsize(text + '..', font=font)[0] > 300:
+                text = text[:-1]
+            text += '..'
+
+        w, h = draw.textsize(text, font=font)
+        draw.text((400/2-w/2, 0), text, (255, 255, 255), font=font)
+
+        img = ImageTk.PhotoImage(img)
+        txt_imgs.append(img)
+
+        if index == 0:
+            txt_ids.append(canvas.create_image(100, 60, image=img, anchor=tkinter.NW))
+        elif index == 1:
+            txt_ids.append(canvas.create_image(100, 160, image=img, anchor=tkinter.NW))
+        elif index == 2:
+            txt_ids.append(canvas.create_image(100, 260, image=img, anchor=tkinter.NW))
+        elif index == 3:
+            txt_ids.append(canvas.create_image(100, 360, image=img, anchor=tkinter.NW))
+
+        return img
+
+    def clear_page():
+
+        for detail_btn in detail_btns:
+            base.canvas.itemconfigure(detail_btn, state='hidden')
+
+        for open_btn in open_btns:
+            base.canvas.itemconfigure(open_btn, state='hidden')
+
+        for empty_txt in empty_txts:
+            base.canvas.itemconfigure(empty_txt, state='hidden')
+
+        for txt_id in txt_ids:
+            base.canvas.delete(txt_id)
+
+        txt_imgs.clear()
+
+
+    def display_page(page_number, groups):
+
+        clear_page()
+
+        items = groups[4*page_number:4*(page_number+1)]
+
+        i = -1
+
+        for i in range(len(items)):
+
+            base.canvas.itemconfigure(detail_btns[i], state='normal')
+            base.canvas.itemconfigure(open_btns[i], state='normal')
+            
+            def gen(i):
+                def clicked_open(event):
+                    open_chat(items[i][1], items[i][0].title)
+                return clicked_open
+
+            base.canvas.tag_bind(open_btns[i], '<Button-1>', gen(i))
+
+            create_text_image(base.canvas, items[i][0].title, i, txt_imgs, txt_ids)
+
+        for i in range(i+1, 4):
+            base.canvas.itemconfigure(empty_txts[i], state='normal')
+
+
+    tile_img = Image.open('tile.jpeg')
+    tile_img = resize_to_spill(tile_img, 400, 80)
+    tile_img = crop_center_align(tile_img, 400, 80)
+    tile_img = ImageTk.PhotoImage(tile_img)
+    temp_assets.append(tile_img)
+
+    open_img = tkinter.PhotoImage(file='open.png')
+    temp_assets.append(open_img)
+
+    details_img = tkinter.PhotoImage(file='details.png')
+    temp_assets.append(details_img)
+
+    empty_img = tkinter.PhotoImage(file='empty.png')
+    temp_assets.append(empty_img)
+
+    
+    tiles = []
+    tiles.append(base.canvas.create_image(100, 60, image=tile_img, anchor=tkinter.NW))
+    tiles.append(base.canvas.create_image(100, 160, image=tile_img, anchor=tkinter.NW))
+    tiles.append(base.canvas.create_image(100, 260, image=tile_img, anchor=tkinter.NW))
+    tiles.append(base.canvas.create_image(100, 360, image=tile_img, anchor=tkinter.NW))
+
+    detail_btns = []
+    detail_btns.append(base.canvas.create_image(100+20, 60+42, image=details_img, anchor=tkinter.NW))
+    detail_btns.append(base.canvas.create_image(100+20, 160+42, image=details_img, anchor=tkinter.NW))
+    detail_btns.append(base.canvas.create_image(100+20, 260+42, image=details_img, anchor=tkinter.NW))
+    detail_btns.append(base.canvas.create_image(100+20, 360+42, image=details_img, anchor=tkinter.NW))
+
+    open_btns = []
+    open_btns.append(base.canvas.create_image(100+270, 60+42, image=open_img, anchor=tkinter.NW))
+    open_btns.append(base.canvas.create_image(100+270, 160+42, image=open_img, anchor=tkinter.NW))
+    open_btns.append(base.canvas.create_image(100+270, 260+42, image=open_img, anchor=tkinter.NW))
+    open_btns.append(base.canvas.create_image(100+270, 360+42, image=open_img, anchor=tkinter.NW))
+
+    empty_txts = []
+    empty_txts.append(base.canvas.create_image(100+110, 60+10, image=empty_img, anchor=tkinter.NW))
+    empty_txts.append(base.canvas.create_image(100+110, 160+10, image=empty_img, anchor=tkinter.NW))
+    empty_txts.append(base.canvas.create_image(100+110, 260+10, image=empty_img, anchor=tkinter.NW))
+    empty_txts.append(base.canvas.create_image(100+110, 360+10, image=empty_img, anchor=tkinter.NW))
+
+    txt_imgs = []
+    temp_assets.append(txt_imgs)
+    txt_ids = []
+
+    text = 'loading..'
+    create_text_image(base.canvas, text, 0, txt_imgs, txt_ids)
+    create_text_image(base.canvas, text, 1, txt_imgs, txt_ids)
+    create_text_image(base.canvas, text, 2, txt_imgs, txt_ids)
+    create_text_image(base.canvas, text, 3, txt_imgs, txt_ids)
+
+    right_img = tkinter.PhotoImage(file='right.png')
+    temp_assets.append(right_img)
+    right_btn = base.canvas.create_image(510, 250, image=right_img, anchor=tkinter.NW)
+
+    def clicked_right(event):
+        nonlocal current_page_number, groups
+        current_page_number += 1
+
+        display_page(current_page_number, groups)
+
+    base.canvas.tag_bind(right_btn, '<Button-1>', clicked_right)
+
+    left_img = tkinter.PhotoImage(file='left.png')
+    temp_assets.append(left_img)
+    left_btn = base.canvas.create_image(30, 250, image=left_img, anchor=tkinter.NW)
+
+    def clicked_left(event):
+        nonlocal current_page_number, groups
+        current_page_number -= 1
+
+        display_page(current_page_number, groups)
+
+    base.canvas.tag_bind(left_btn, '<Button-1>', clicked_left)
+
+    back_img = tkinter.PhotoImage(file='back.png')
+    temp_assets.append(back_img)
+    back_btn = base.canvas.create_image(900, 20, image=back_img, anchor=tkinter.NW)
+
+    def clicked_back(event):
+        cleanup()
+        category_selection_page(base, temp_assets)
+
+    base.canvas.tag_bind(back_btn, '<Button-1>', clicked_back)
+
+    groups = server_interface.get_groups()
+    groups = [(groups[i], i) for i in range(len(groups)) if groups[i].is_member == 'yes']
+    current_page_number = 0
+    display_page(current_page_number, groups)
+
+
+def open_chat(room_number, name):
+
+    def send(event=None):
+        msg = entry_field.get()
+        msg_list.insert(tkinter.END, f'[{username}]: ' + msg)
+        entry_field.delete(0, tkinter.END)
+        server_interface.send_chat(room_number, msg)
+
+    top = tkinter.Tk()
+    top.title(name + ': discussion')
+
+    messages_frame = tkinter.Frame(top)
+    scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+    msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+    scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+    messages_frame.pack()
+
+    entry_field = tkinter.Entry(top)
+    entry_field.bind("<Return>", send)
+    entry_field.pack()
+    send_button = tkinter.Button(top, text="Send", command=send)
+    send_button.pack()
+
+    chat = server_interface.get_chat(room_number)
+    chat = chat.split('\n')
+    chat = [msg for msg in chat if msg]
+    print(chat)
+
+    for msg in chat:
+        msg_list.insert(tkinter.END, msg)
+
+    top.mainloop()
+
+
+
+
 
 
 if __name__ == '__main__':
---
+++ client/server.py
@@ -0,0 +1,179 @@
+# simple echo server
+
+import socket
+import threading
+
+PORT = 5200
+
+
+rooms = []
+
+class Room():
+
+    def __init__(self, title, tags, goals):
+        self.title = title
+        self.tags = tags
+        self.goals = goals
+        self.followers = set()
+        self.chat = ''
+
+class Server():
+
+    def __init__(self, host, port):
+
+        # server address
+        self.host = host
+        self.port = port
+
+        # create socket
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # IPv4, TCP
+
+        # socket option to reuse idle socket
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # bind socket to address
+        self.socket.bind((self.host, self.port))
+
+
+    def listen(self):
+
+        # start listening for client connections
+        self.socket.listen(100)
+            # max 100 pending connections
+
+        print('server listening..\n')
+
+        while True:
+
+            # accept new client connection
+            client, address = self.socket.accept()
+
+            print('client accepted\n')
+
+            # handle client in separate thread
+            handler_thread = threading.Thread(target=self.client_handler, args=(client,))
+            handler_thread.start()
+
+    
+    def client_handler(self, client):
+
+        def can_read():
+
+            decoded_buffer = buffer.decode()
+
+            pos = decoded_buffer.find('\n')
+            if pos == -1:
+                return False
+
+            size = int(decoded_buffer[:pos].split()[1])
+
+            if len(decoded_buffer) - (pos+1) >= size:
+                return True
+            return False
+
+        def read():
+
+            nonlocal buffer
+
+            decoded_buffer = buffer.decode()
+
+            pos = decoded_buffer.find('\n')
+            size = int(decoded_buffer[:pos].split()[1])
+
+            message = decoded_buffer[pos+1:size+(pos+1)]
+            buffer = decoded_buffer[size+(pos+1):].encode()
+
+            handle_message(message)
+
+        def handle_message(message):
+            
+            pos = message.find('\n')
+            if pos == -1:
+                pass
+
+            type = message[:pos].split()[1]
+            message = message[pos+1:]
+
+            if type == 'username':
+                nonlocal client_username
+                client_username = message.split()[1]
+
+            elif type == 'create':
+                parts = message.split()
+                
+                title = parts[1]
+                tags = parts[3]
+                goals = parts[5]
+
+                room = Room(title, tags, goals)
+                room.followers.add(client_username)
+                room.chat = 'Welcome to the chat!\nFeel free to participate!\n'
+
+                rooms.append(room)
+
+            elif type == 'rooms':
+
+                load = ''
+
+                for room in rooms:
+                    load += room.title
+                    load += '\n'
+                    load += room.tags
+                    load += '\n'
+                    load += room.goals
+                    load += '\n'
+                    load += 'yes' if client_username in room.followers else 'no'
+                    load += '\n'
+
+                client.send((f'size: {len(load)}\n' + load).encode())
+
+            elif type == 'read':
+                room_number = int(message.split()[1])
+
+                load = rooms[room_number].chat
+
+                client.send((f'size: {len(load)}\n' + load).encode())
+
+            elif type == 'send':
+                pos = message.find('\n')
+                room_number = int(message[:pos].split()[1])
+
+                message = message[pos+1:]
+
+                rooms[room_number].chat += f'[{client_username}]: {message}\n'
+
+
+
+        client_username = ''
+        
+        buffer = b''
+        recv_size = 1024
+
+        while True:
+
+            #try:
+                # recieve data from user
+                data = client.recv(recv_size)
+                print(f'received: {data}')
+
+                if data:
+                    buffer += data
+                    if can_read():
+                        read()
+
+                else:
+                    raise error('disconnected')
+
+            #except:
+                # close the socket
+            #    client.close()
+            #    print('client socket closed\n')
+
+
+
+
+# if run as script
+if __name__ == "__main__":
+    server = Server('', PORT)
+    server.listen()
